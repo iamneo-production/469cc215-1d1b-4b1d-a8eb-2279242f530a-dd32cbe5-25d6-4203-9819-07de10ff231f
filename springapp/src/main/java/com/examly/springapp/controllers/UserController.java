76c2ab879b57d70319bbf23a37905c60f4ea6ae0
superdemo09 <superdemo72@gmail.com>
2023-09-19 17:48:55 +0000

    commite main

--- springapp/src/main/java/com/examly/springapp/controllers/UserController.java
+++ springapp/src/main/java/com/examly/springapp/controllers/UserController.java
@@ -47,6 +47,13 @@ public class UserController {
 
 		// Return null if the provided credentials don't match any user
 		return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+        System.out.println("hai");
+        System.out.println("1");
+        System.out.println("2");
+        System.out.println("3");
+        System.out.println("4");
+        System.out.println("5");
+
 	}
 	
 
